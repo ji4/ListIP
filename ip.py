452dbdda492711e1a9c1a049a48b427bf8dd4303
ji4 <money@Anonymousde-MacBook-Air.local>
2018-04-22 18:05:48 +0800

    Filter Local Dst IP

--- ip.py
+++ ip.py
@@ -6,7 +6,7 @@ from os import listdir
 from os.path import isfile, join
 
 path = "/Users/money/Downloads/0413_三國志_Android"
-filter = "'ip.src >= 192.168.137.2 && ip.src <= 192.168.137.255 && ip.dst != 192.168.137.1 && not icmp.type == 3'"
+filter = "'ip.src >= 192.168.137.2 && ip.src <= 192.168.137.255 && not (ip.dst >= 192.168.0.0 && ip.dst <= 192.168.255.255) && not icmp.type == 3'"
 fileNames = ['FbLogin', 'FbPersonal',
             'GoogleLogin','GooglePersonal',
             'GuestLogin', 'GuestPersonal',
@@ -38,7 +38,7 @@ def listAllIps():
                 if(len(lineIp) > 0):
                     fCategory_ip.write(file.split('.')[fileNameIndex] + '\t' + lineIp.split('\t')[dstIpIndex] + '\n')
 def sortAllData():
-    with open('category_ip_unsorted', 'r') as fOldCategory_ip, open('category_sorted', 'a+') as fNewCategory_ip:
+    with open('category_ip_unsorted', 'r') as fOldCategory_ip, open('category_ip_sorted', 'a+') as fNewCategory_ip:
         categoryIps = fOldCategory_ip.read()
         content = set()
         for fileName in fileNames:
@@ -47,6 +47,7 @@ def sortAllData():
                 if line.startswith(fileName) and appendedline not in content:
                     fNewCategory_ip.write(appendedline + '\n')
                     content.add(appendedline)
+    os.system('rm category_ip_unsorted')
             
 if __name__ == "__main__":
     pcapToTxt()
